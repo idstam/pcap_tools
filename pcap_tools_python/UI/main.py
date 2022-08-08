092e94ebb33eee47eb87655753ffaf4e47d697b4
Johan Idstam <johan@idstam.com>
2022-08-08 15:37:49 +0200

    WIP

--- pcap_tools_python/UI/main.py
+++ pcap_tools_python/UI/main.py
@@ -4,4 +4,22 @@ import PySimpleGUI as sg
 class MainWindow:
 
     def __init__(self):
-        sg.Window(title="Hello World", layout=[[]], margins=(100, 50)).read()
+        layout = [[sg.T("")], [sg.Text("Choose a file: "), sg.Input(), sg.FileBrowse(key="-IN-"), sg.Button("OK")]]
+
+
+
+        # Create the window
+        window = sg.Window("Demo", layout)
+
+        # Create an event loop
+        while True:
+            event, values = window.read()
+            # End program if user closes window or
+            # presses the OK button
+            if event == "OK" or event == sg.WIN_CLOSED:
+                print(values["-IN-"])
+                break
+
+
+
+        window.close()
